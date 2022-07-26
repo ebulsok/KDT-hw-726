6953a3f6c9ebc93e1392e80c1e1bc0a81a29599b
ebulsok <leebulsok@gmail.com>
2022-07-27 00:35:53 +0900

    Edit burgermenu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const snbMenu = document.querySelector("#snb .submenu");
+const snbMenu = document.querySelector(".submenu");
 const snbBtn = document.querySelector("#snb .btn_menu");
+const xBtn = document.querySelector(".submenu .xmark")
 
 snbBtn.addEventListener("click", function() {
-    if(snbMenu.classList.contains("hide")) snbMenu.classList.remove("hide");
-    else snbMenu.classList.add("hide");
+    if(snbMenu.classList.contains("hide")){
+        snbMenu.classList.remove("hide");
+        snbBtn.classList.add("hide");
+    }
+})
+
+xBtn.addEventListener("click", function() {
+    if(!snbMenu.classList.contains("hide")){
+        snbMenu.classList.add("hide");
+        snbBtn.classList.remove("hide");
+    }
 })
